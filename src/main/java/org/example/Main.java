7271c2e7154fe3351daa23bf3ab679d6cb04faa9
PrasanaBabu <88841365+PrasanaBabu@users.noreply.github.com>
2022-11-30 10:44:44 +0530

    Handled Exceptions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,33 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter number");
-        int input = sc.nextInt();
+            try {
+                int input = sc.nextInt();
+                if(input<0)
+                    throw new NegativeNumberException("Please give Positive numbers");
+                FizzBuzz fb = new FizzBuzz();
 
-        FizzBuzz fb = new FizzBuzz();
+                String output = fb.fizzbuzz(input);
+                System.out.println("output = " + output);
 
-        String output = fb.fizzbuzz(input);
-        System.out.println("output = " + output);
+                System.out.println("Enter limit");
+                int limit = sc.nextInt();
 
-        System.out.println("Enter limit");
-        int limit = sc.nextInt();
+                List<String> outputStrings = fb.fizzbuzzTillLimit(limit);
 
-        List<String> outputStrings = fb.fizzbuzzTillLimit(limit);
+                System.out.println("outputStrings = " + outputStrings);
 
-        System.out.println("outputStrings = " + outputStrings);
-
-
+            } catch (NegativeNumberException e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Input Mismatch. Please enter only integer" );
+            }
     }
 }
